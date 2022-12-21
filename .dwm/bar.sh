94b50915819052d16f132b1520e11aa0990f5fa8
Raghuram Subramani <raghus2247@gmail.com>
2022-12-21 14:38:38 +0530

    delete .dwm

--- .dwm/bar.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-	xsetroot -name "| $(if [ $(iwgetid -r) ]; then echo " $(iwgetid -r)"; else echo ' disconnected'; fi) |  $(cat /sys/class/power_supply/BAT1/capacity)% | $(date +' %d/%m/%y |  %A, %R') | λ $(whoami)"
-	sleep 5
-done
--- .dwm/dmenu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-j4-dmenu-desktop --dmenu="dmenu -i -fn 'monospace:size=10' -nb '#131417' -nf '#bbbbbb' -sb '#d3d7f2' -sf '#1e1f26'"
