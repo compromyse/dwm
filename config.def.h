f40c3e89f0a9f86bedf13b8b18167b1f1f434694
Raghuram Subramani <raghus2247@gmail.com>
2023-08-05 13:15:40 +0530

    systray icon size

--- config.def.h
+++ config.def.h
@@ -7,6 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 1;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayiconsize = 16; /* systray icon size in px */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -33,6 +34,8 @@ static const char *const autostart[] = {
   "wmname", "LG3D", NULL,
   "/home/compromyse/.dwm/bar.sh", NULL,
   "xss-lock", "-l", "--", "xsecurelock", NULL,
+  "blueman-applet", NULL,
+  "nm-applet", NULL,
  	NULL /* terminate */
 };
 
